ee0c53ad90783a787985da3f9c2ce355e69887bd
ddelfaus <ddelfaus@gmail.com>
2019-11-01 12:27:10 -0400

    useing a custom hook

--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PlayerTable from './PlayerTable'
-
+import {Toggle } from "../hooks/Toggle"
 
 const Players = props => {
+
+const [order, setOrder] = Toggle(true)
 
 console.log(props)
 
 
+const toggleMode = () => {
+   if (order = false){
+      props.players.reverse();
+  
+   }
+}
+
     return(
         <div>
-            {props.players.map(item =>(
+            <button onClick ={setOrder} >
+            {order ? `Order`: "reverse Order"}
+            </button>
+            { props.players.map(item =>(
                 <PlayerTable key ={item.id} players ={item}/>
             ))}
+                
+          
         </div>
     )
 }
